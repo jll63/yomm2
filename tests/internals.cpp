a7cbf8debd92252b957f3d11a3fe7134e110cbed
Jean-Louis Leroy <jl@leroy.nyc>
2017-10-16 12:32:07 -0400

    inception

---
+++ tests/internals.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <type_traits>
+
+#include <yorel/openmethods.hpp>
+
+using std::is_same;
+namespace yomm = yorel::openmethods;
+
+struct Matrix {
+    virtual ~Matrix() {}
+};
+
+struct Dense_matrix : Matrix {};
+struct Diagonal_matrix : Matrix {};
+
+declare_method(void, times, const Matrix*, const Matrix*);
+declare_method(void, times, double, const Matrix*);
+declare_method(void, times, const Matrix*, double);
+
+begin_method(void, times, const Matrix*, const Matrix*) {
+} end_method;
+
+int main()
+{
+    const Matrix& dense = Dense_matrix();
+    const Matrix& diag = Diagonal_matrix();
+    times(&dense, &dense);
+    times(2, &dense);
+    times(&dense, 2);
+    times(&diag, &dense);
+    times(&diag, &diag);
+    //static_assert(is_same<yomm::Method<>, times_spec<void()>>::value, "failure");
+}
