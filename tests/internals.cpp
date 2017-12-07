b26042a8ad13d926c82989e51167cb771f22b676
Jean-Louis Leroy <jl@leroy.nyc>
2017-12-07 06:37:32 -0500

    introduce boost test

--- tests/internals.cpp
+++ tests/internals.cpp
@@ -3,8 +3,12 @@
 
 #include <yorel/yomm2.hpp>
 
+#define BOOST_TEST_MODULE example
+#include <boost/test/included/unit_test.hpp>
+
 using std::is_same;
-using yorel::yomm2::virtual_;
+namespace yomm2 = yorel::yomm2;
+using yomm2::virtual_;
 
 struct matrix {
     virtual ~matrix() {}
@@ -19,9 +23,27 @@ init_class_info<matrix> i0(global_class_registry());
 init_class_info<dense_matrix, matrix> i2(global_class_registry());
 init_class_info<diagonal_matrix, matrix> i3(global_class_registry());
 
-YOMM2_DECLARE(void, times, virtual_<const matrix&>, virtual_<const matrix&>);
-YOMM2_DECLARE(void, times, double, virtual_<const matrix&>);
-YOMM2_DECLARE(void, times, virtual_<const matrix&>, double);
+// int main()
+// {
+//     yorel::yomm2::update_methods();
+
+//     const matrix& dense = dense_matrix();
+//     const matrix& diag = diagonal_matrix();
+//     times(dense, dense);
+//     times(2, dense);
+//     times(dense, 2);
+//     times(diag, dense);
+//     times(diag, diag);
+// }
+
+namespace test_internals {
+
+struct test;
+auto& registry = yomm2::registry::get<test>();
+
+YOMM2_DECLARE_(test, void, times, virtual_<const matrix&>, virtual_<const matrix&>);
+YOMM2_DECLARE_(test, void, times, double, virtual_<const matrix&>);
+YOMM2_DECLARE_(test, void, times, virtual_<const matrix&>, double);
 
 YOMM2_DEFINE(void, times, const matrix&, const matrix&) {
 } YOMM2_END;
@@ -41,15 +63,9 @@ YOMM2_DEFINE(void, times, const diagonal_matrix& m, double a) {
 YOMM2_DEFINE(void, times, const matrix& m, double a) {
 } YOMM2_END;
 
-int main()
+BOOST_AUTO_TEST_CASE(registration)
 {
-    yorel::yomm2::update_methods();
+    BOOST_TEST(registry.methods.size() == 3);
+}
 
-    const matrix& dense = dense_matrix();
-    const matrix& diag = diagonal_matrix();
-    times(dense, dense);
-    times(2, dense);
-    times(dense, 2);
-    times(diag, dense);
-    times(diag, diag);
 }
