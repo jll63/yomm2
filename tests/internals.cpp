0cb8d410e037ea3898a0aea0755a0be7a4b246f3
Jean-Louis Leroy <jl@leroy.nyc>
2017-12-09 14:25:19 -0500

    rolex

--- tests/internals.cpp
+++ tests/internals.cpp
@@ -12,7 +12,7 @@ using std::is_same;
 namespace yomm2 = yorel::yomm2;
 using yomm2::virtual_;
 
-namespace test_internals {
+namespace matrices {
 
 struct test;
 auto& registry = yomm2::registry::get<test>();
@@ -80,6 +80,120 @@ BOOST_AUTO_TEST_CASE(registration)
 
     BOOST_TEST(registry.methods[2]->specs.size() == 2);
     BOOST_TEST(registry.methods[2]->vargs.size() == 1);
+}
+
+}
+
+namespace rolex {
+
+struct test;
+auto& registry = yomm2::registry::get<test>();
+
+struct role {
+    virtual ~role() {}
+};
+
+struct employee : role {};
+struct executive : employee {};
+struct founder : role {};
+
+struct expense {
+    ~expense() {}
+};
+
+struct public_transport : expense {};
+struct cab : public_transport {};
+struct bus : public_transport {};
+struct jet : expense {};
+
+YOMM2_CLASS_(test, role);
+YOMM2_CLASS_(test, employee, role);
+YOMM2_CLASS_(test, executive, employee);
+YOMM2_CLASS_(test, founder, role);
+YOMM2_CLASS_(test, expense);
+YOMM2_CLASS_(test, public_transport, expense);
+YOMM2_CLASS_(test, cab, public_transport);
+YOMM2_CLASS_(test, bus, public_transport);
+YOMM2_CLASS_(test, jet, expense);
+
+YOMM2_DECLARE_(test, double, pay, virtual_<const role&>);
+YOMM2_DECLARE_(test, bool, approve, virtual_<const role&>, virtual_<const expense&>, double);
+
+YOMM2_DEFINE(double, pay, const employee&) {
+    return 2000;
+} YOMM2_END;
+
+YOMM2_DEFINE(double, pay, const executive&) {
+    return 5000;
+} YOMM2_END;
+
+YOMM2_DEFINE(double, pay, const founder&) {
+    return 0;
+} YOMM2_END;
+
+YOMM2_DEFINE(bool, approve, const role& r, const expense& e, double amount) {
+    return false;
+} YOMM2_END;
+
+YOMM2_DEFINE(bool, approve, const founder& r, const expense& e, double amount) {
+    return true;
+} YOMM2_END;
+
+YOMM2_DEFINE(bool, approve, const employee& r, const public_transport& e, double amount) {
+    return true;
+} YOMM2_END;
+
+YOMM2_DEFINE(bool, approve, const executive& r, const jet& e, double amount) {
+    return true;
+} YOMM2_END;
+
+BOOST_AUTO_TEST_CASE(registration)
+{
+    BOOST_TEST_REQUIRE(registry.classes.size() == 9);
+
+    auto class_iter = registry.classes.begin();
+    auto role_class = *class_iter++;
+    auto employee_class = *class_iter++;
+    auto executive_class = *class_iter++;
+    auto founder_class = *class_iter++;
+    auto expense_class = *class_iter++;
+    auto public_transport_class = *class_iter++;
+    auto cab_class = *class_iter++;
+    auto bus_class = *class_iter++;
+    auto jet_class = *class_iter++;
+
+    BOOST_TEST(role_class->bases.size() == 0);
+
+    BOOST_TEST_REQUIRE(employee_class->bases.size() == 1);
+    BOOST_TEST(employee_class->bases[0] == role_class);
+
+    BOOST_TEST_REQUIRE(executive_class->bases.size() == 1);
+    BOOST_TEST(executive_class->bases[0] == employee_class);
+
+    BOOST_TEST_REQUIRE(founder_class->bases.size() == 1);
+    BOOST_TEST(founder_class->bases[0] == role_class);
+
+    BOOST_TEST_REQUIRE(public_transport_class->bases.size() == 1);
+    BOOST_TEST(public_transport_class->bases[0] == expense_class);
+
+    BOOST_TEST_REQUIRE(cab_class->bases.size() == 1);
+    BOOST_TEST(cab_class->bases[0] == public_transport_class);
+
+    BOOST_TEST_REQUIRE(bus_class->bases.size() == 1);
+    BOOST_TEST(bus_class->bases[0] == public_transport_class);
+
+    BOOST_TEST_REQUIRE(jet_class->bases.size() == 1);
+    BOOST_TEST(jet_class->bases[0] == expense_class);
+
+    BOOST_TEST_REQUIRE(registry.methods.size() == 2);
+
+    auto pay_method = registry.methods[0];
+    BOOST_TEST(pay_method->vargs.size() == 1);
+    BOOST_TEST(pay_method->specs.size() == 3);
+
+    auto approve_method = registry.methods[1];
+    BOOST_TEST(approve_method->vargs.size() == 2);
+    BOOST_TEST(approve_method->specs.size() == 4);
 }
 
 }
