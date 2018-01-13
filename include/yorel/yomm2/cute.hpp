6d0a6c3daed719627f297aacbc1e3ea51bbcc220
Jean-Louis Leroy <jl@leroy.nyc>
2018-01-13 04:53:12 -0500

    cute

---
+++ include/yorel/yomm2/cute.hpp
@@ -0,0 +1,11 @@
+#ifndef YOREL_YOMM2_CUTE_INCLUDED
+#define YOREL_YOMM2_CUTE_INCLUDED
+
+#include <yorel/yomm2.hpp>
+
+#define declare_method YOMM2_DECLARE
+#define begin_method YOMM2_DEFINE
+#define end_method YOMM2_END
+#define register_class YOMM2_CLASS
+
+#endif
