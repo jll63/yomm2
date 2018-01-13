ed59b199f4f074c239ea8bb78df3a5d321ca13d0
Jean-Louis Leroy <jl@leroy.nyc>
2018-01-13 18:28:58 -0500

    license

--- src/yomm2.cpp
+++ src/yomm2.cpp
@@ -1,7 +1,7 @@
-// Copyright (c) 2013 Jean-Louis Leroy
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// Copyright (c) 2018 Jean-Louis Leroy
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <yorel/yomm2.hpp>
 #include <yorel/yomm2/runtime.hpp>
--- tests/benchmark_matrices.hpp
+++ tests/benchmark_matrices.hpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2018 Jean-Louis Leroy
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <yorel/yomm2.hpp>
 #include <benchmark/benchmark.h>
 
