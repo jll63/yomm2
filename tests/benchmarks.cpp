0309e35c1c5d025fe2a0eb50e825faca30d13434
Jean-Louis Leroy <jl@leroy.nyc>
2018-01-14 15:40:04 -0500

    add benchmark results

--- tests/benchmarks.cpp
+++ tests/benchmarks.cpp
@@ -36,3 +36,42 @@ int main(int argc, char** argv) {
     benchmark::RunSpecifiedBenchmarks();
     return 0;
 }
+
+#if 0
+
+Run on (8 X 3500 MHz CPU s)
+CPU Caches:
+  L1 Data 32K (x4)
+  L1 Instruction 32K (x4)
+  L2 Unified 256K (x4)
+  L3 Unified 6144K (x1)
+
+===================================================================================
+g++ (Ubuntu 7.2.0-8ubuntu3) 7.2.0
+-----------------------------------------------------------------------------------
+Benchmark                                            Time           CPU Iterations
+-----------------------------------------------------------------------------------
+virtual function call                                2 ns          2 ns  393105986
+uni-method call                                      2 ns          2 ns  339690614
+double dispatch                                      2 ns          2 ns  395433996
+multi-method call                                    3 ns          3 ns  212230988
+virtual function call (virtual inheritance)          2 ns          2 ns  394559425
+uni-method call (virtual inheritance)                2 ns          2 ns  298056685
+double dispatch(virtual inheritance)                 2 ns          2 ns  341053349
+multi-method call (virtual inheritance)              3 ns          3 ns  212678531
+
+===================================================================================
+clang version 5.0.0-3 (tags/RELEASE_500/final)
+-----------------------------------------------------------------------------------
+Benchmark                                            Time           CPU Iterations
+-----------------------------------------------------------------------------------
+virtual function call                                2 ns          2 ns  393459076
+uni-method call                                      2 ns          2 ns  335140869
+double dispatch                                      2 ns          2 ns  296683771
+multi-method call                                    3 ns          3 ns  208823581
+virtual function call (virtual inheritance)          2 ns          2 ns  392972597
+uni-method call (virtual inheritance)                2 ns          2 ns  296582445
+double dispatch(virtual inheritance)                 2 ns          2 ns  340165177
+multi-method call (virtual inheritance)              3 ns          3 ns  203740055
+
+#endif
