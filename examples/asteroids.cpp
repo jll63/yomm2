72a7a777fa99d76514df0a24ba96c84982c35c44
Jean-Louis Leroy <jl@leroy.nyc>
2018-01-31 17:04:11 -0500

    asteroids

---
+++ examples/asteroids.cpp
@@ -0,0 +1,63 @@
+// asteroids.cpp
+// Copyright (c) 2018 Jean-Louis Leroy
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// Example for Wikipedia
+
+#include <yorel/yomm2/cute.hpp>
+
+using yorel::yomm2::virtual_;
+
+class Thing {
+  public:
+    virtual ~Thing() {}
+};
+
+class Asteroid : public Thing {
+};
+
+class Spaceship : public Thing {
+};
+
+register_class(Thing);
+register_class(Spaceship, Thing);
+register_class(Asteroid, Thing);
+
+declare_method(void, collideWith, (virtual_<Thing&>, virtual_<Thing&>));
+
+define_method(void, collideWith, (Thing& left, Thing& right)) {
+    // default collision handling
+}
+
+define_method(void, collideWith, (Asteroid& left, Asteroid& right)) {
+    // handle Asteroid-Asteroid collision
+}
+
+define_method(void, collideWith, (Asteroid& left, Spaceship& right)) {
+    // handle Asteroid-Spaceship collision
+}
+
+define_method(void, collideWith, (Spaceship& left, Asteroid& right)) {
+    // handle Spaceship-Asteroid collision
+}
+
+define_method(void, collideWith, (Spaceship& left, Spaceship& right)) {
+    // handle Spaceship-Spaceship collision
+}
+
+int main() {
+    yorel::yomm2::update_methods();
+
+    Asteroid  a1, a2;
+    Spaceship s1, s2;
+
+    collideWith(a1, a2);
+    collideWith(a1, s1);
+
+    collideWith(s1, s2);
+    collideWith(s1, a1);
+
+    return 0;
+}
