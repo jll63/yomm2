1589cd93953c6946764cd95f1aa0b187684442b3
Jean-Louis Leroy <jl@leroy.nyc>
2020-01-29 20:37:29 -0500

    create lab

---
+++ tests/lab.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2020 Jean-Louis Leroy
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <yorel/yomm2/cute.hpp>
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+using yorel::yomm2::virtual_;
+using std::cout;
+
+// register_class(class, bases);
+
+// declare_method(return, name, (params));
+
+// define_method(return, name, (params)) {
+// }
+
+int main() {
+    yorel::yomm2::update_methods();
+    return 0;
+}
