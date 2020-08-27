e61f0706dc04dbc0790082f7184848cadcc1c9e9
Peter Spalthoff <peter.spalthoff@gmail.com>
2020-08-27 14:40:04 +0900

    Improvements to find_and_download

--- cmake/find_or_download_package.cmake
+++ cmake/find_or_download_package.cmake
@@ -1,13 +1,28 @@
 macro(find_or_download_package PACKAGE)
-  find_package(${PACKAGE} QUIET)
+  set(options EXACT PKG_CONFIG INSTALL_WITH_YOMM)
+  set(oneValueArgs VERSION)
+  cmake_parse_arguments(ARGS
+    "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
+  )
+  set(DEPENDENCY_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/dependencies/${PACKAGE})
+  find_package(
+    ${PACKAGE} QUIET
+    HINTS ${DEPENDENCY_INSTALL_PREFIX} ${CMAKE_INSTALL_PREFIX}
+  )
   if(NOT ${${PACKAGE}_FOUND})
     message(STATUS "Package \"${PACKAGE}\" not found in system.")
     message(STATUS
       "Downloading dependency \"${PACKAGE}\" and building from source."
     )
-
-    set(DEPENDENCY_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/dependencies)
-
+    # Use below settings for git downloads if available
+    if(${CMAKE_VERSION} VERSION_GREATER 3.6)
+      list(APPEND ADDITIONAL_GIT_SETTINGS "GIT_SHALLOW True")
+    endif()
+    if(${CMAKE_VERSION} VERSION_GREATER 3.8)
+      list(APPEND ADDITIONAL_GIT_SETTINGS
+        "GIT_PROGRESS True GIT_CONFIG advice.detachedHead=false"
+      )
+    endif()
     # Prepare download instructions for dependency
     configure_file(
       ${CMAKE_SOURCE_DIR}/cmake/${PACKAGE}_download.cmake.in
@@ -36,12 +51,18 @@ macro(find_or_download_package PACKAGE)
       message(FATAL_ERROR "Build of  dependency ${PACKAGE} failed: ${result}")
     endif()
 
-    # Update search path and use regular find_package to add dependency
-    # TODO Use same directory here as for configure_file up there and inside
-    # download instructions!
-
+    # Restrict search path and use regular find_package to add dependency
     find_package(${PACKAGE}
       REQUIRED NO_DEFAULT_PATH PATHS "${DEPENDENCY_INSTALL_PREFIX}"
     )
+
+    # Install the built package alongside YOMM if so desired, by copying the
+    # install made in the build tree
+    if(${ARGS_INSTALL_WITH_YOMM})
+      install(
+        DIRECTORY ${DEPENDENCY_INSTALL_PREFIX}/
+        DESTINATION ${CMAKE_INSTALL_PREFIX}
+      )
+    endif()
   endif()
 endmacro()
