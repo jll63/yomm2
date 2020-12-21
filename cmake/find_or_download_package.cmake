648eb0ef2b96cf6a4328f162134b5eea93e351b7
Peter Spalthoff <peter.spalthoff@gmail.com>
2020-12-22 01:58:16 +0900

    Install of Boost delayed until install of yomm

--- cmake/find_or_download_package.cmake
+++ cmake/find_or_download_package.cmake
@@ -2,33 +2,32 @@ macro(find_or_download_package PACKAGE)
   set(options EXACT PKG_CONFIG INSTALL_WITH_YOMM)
   set(oneValueArgs VERSION)
   cmake_parse_arguments(ARGS
-      "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
+    "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
   )
-
+  set(DEPENDENCY_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/dependencies/${PACKAGE})
+  if(${ARGS_INSTALL_WITH_YOMM})
+    install(
+      DIRECTORY ${DEPENDENCY_INSTALL_PREFIX}/
+      DESTINATION ${CMAKE_INSTALL_PREFIX}
+    )
+  endif()
   find_package(
     ${PACKAGE} QUIET
-    HINTS ${CMAKE_SOURCE_DIR}/dependencies ${CMAKE_INSTALL_PREFIX}
+    HINTS ${DEPENDENCY_INSTALL_PREFIX} ${CMAKE_INSTALL_PREFIX}
   )
-
-  if(${ARGS_INSTALL_WITH_YOMM})
-      set(DEPENDENCY_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
-  else()
-      set(DEPENDENCY_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/dependencies)
-  endif()
   if(NOT ${${PACKAGE}_FOUND})
     message(STATUS "Package \"${PACKAGE}\" not found in system.")
     message(STATUS
       "Downloading dependency \"${PACKAGE}\" and building from source."
     )
-
     # Use below settings for git downloads if available
     if(${CMAKE_VERSION} VERSION_GREATER 3.6)
-        list(APPEND ADDITIONAL_GIT_SETTINGS "GIT_SHALLOW True")
+      list(APPEND ADDITIONAL_GIT_SETTINGS "GIT_SHALLOW True")
     endif()
     if(${CMAKE_VERSION} VERSION_GREATER 3.8)
-        list(APPEND ADDITIONAL_GIT_SETTINGS
-            "GIT_PROGRESS True GIT_CONFIG advice.detachedHead=false"
-        )
+      list(APPEND ADDITIONAL_GIT_SETTINGS
+        "GIT_PROGRESS True GIT_CONFIG advice.detachedHead=false"
+      )
     endif()
     # Prepare download instructions for dependency
     configure_file(
