127b1c5969fdcbe668695e13266107f8abb47522
Jean-Louis Leroy <jl@leroy.nyc>
2020-12-26 12:52:04 -0500

    docker image for testing build and installation

---
+++ docker/yomm2-init-image.sh
@@ -0,0 +1,23 @@
+user=$1
+shift
+uid=$1
+shift
+group=$1
+shift
+gid=$1
+shift
+
+mkdir /etc/sudoers.d
+echo "ALL ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/sudoall
+chmod 0440 /etc/sudoers.d/sudoall
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y tzdata
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt-get install -y cmake git g++ sudo
+
+groupadd -g $gid $group
+useradd -g $gid -m $user
