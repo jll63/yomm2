0b3e17049d7fbcdcbbcb8e2f65aefa0e4851cb0e
Jean-Louis Leroy <jl@leroy.nyc>
2021-08-07 11:21:04 -0400

    update copyright notice

--- examples/next.cpp
+++ examples/next.cpp
@@ -1,5 +1,5 @@
 // next.cpp
-// Copyright (c) 2018-2020 Jean-Louis Leroy
+// Copyright (c) 2018-2021 Jean-Louis Leroy
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
