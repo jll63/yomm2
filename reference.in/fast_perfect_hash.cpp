5f958f4f70da0d29af3d1b6bfeca5a860c0e2368
Jean-Louis Leroy <jl@leroy.nyc>
2024-02-19 11:23:00 +0800

    doc

--- reference.in/fast_perfect_hash.cpp
+++ reference.in/fast_perfect_hash.cpp
@@ -5,8 +5,7 @@
 
 <sub>/ ->home / ->reference </sub>
 
-entry: yorel::yomm2::policy::fast_perfect_hash
-hrefs: checked_fast_perfect_hash
+entry: yorel::yomm2::policy::fast_perfect_hash hrefs: checked_fast_perfect_hash
 headers: yorel/yomm2/policy.hpp, yorel/yomm2/core.hpp, yorel/yomm2/keywords.hpp
 
 ---
@@ -14,56 +13,84 @@ headers: yorel/yomm2/policy.hpp, yorel/yomm2/core.hpp, yorel/yomm2/keywords.hpp
 struct fast_perfect_hash; struct checked_fast_perfect_hash;
 ```
 
-`fast_perfect_hash` is an implementation of ->`type_hash`. It finds a perfect hash function (i.e. collision-free)
+`fast_perfect_hash` is an implementation of ->`type_hash`. It uses a very fast
+hash function:
+
+```
+H(x) = (x * M) >> N
+```
+
+The `M` and `N` coefficients are determined during initialization so that, for
+the given set of type ids, the hash function is _perfect_, i.e. collision-free.
+It is not _minimal_: there may be gaps in the interval formed by applying the
+hash function to all the known type ids.
 
 ***/
 
-#include <sstream>
+//***
+
 #include <yorel/yomm2/policy.hpp>
 
 // for brevity
 using namespace yorel::yomm2;
 using namespace yorel::yomm2::policy;
 
-struct test : basic_policy<test> {}; // dummy policy
+struct test : basic_policy<test, fast_perfect_hash<test>> {}; // dummy policy
 
 BOOST_AUTO_TEST_CASE(ref_fast_perfect_hash) {
-    std::vector<type_id> ids = {42, 1963, 602701};
-    fast_perfect_hash<test>::hash_initialize(ids.begin(), ids.end());
+  std::vector<type_id> ids = {42, 1963, 2001};
+  test::hash_initialize(ids.begin(), ids.end());
 
-    for (auto id : ids) {
-        BOOST_TEST_MESSAGE(
-            id << " -> " << fast_perfect_hash<test>::hash_type_id(id));
-        // 42 -> 2
-        // 1963 -> 0
-        // 602701 -> 1
+  for (auto id : ids) {
+    BOOST_TEST_MESSAGE(id << " -> " << test::hash_type_id(id));
+    // 42 -> 0
+    // 1963 -> 3
+    // 602701 -> 2
 
-        for (auto other : ids) {
-            BOOST_TEST(
-                (fast_perfect_hash<test>::hash_type_id(id) ==
-                 fast_perfect_hash<test>::hash_type_id(other)) ==
-                (id == other));
-        }
+    for (auto other : ids) {
+      BOOST_TEST((test::hash_type_id(id) == test::hash_type_id(other)) ==
+                 (id == other));
     }
+  }
+}
+//***
 
-    // problem:
-    BOOST_TEST_MESSAGE(
-        666 << " -> " << fast_perfect_hash<test>::hash_type_id(666));
-    // 666 -> 0 !!!
+/***
+Problem
+
+***/
+
+//***
+
+BOOST_AUTO_TEST_CASE(ref_fast_perfect_hash_problem) {
+  BOOST_TEST_MESSAGE(666 << " -> " << test::hash_type_id(666));
+  // 666 -> 3 !!!
 }
 
-struct throw_policy : basic_policy<throw_policy, throw_error_handler> {};
+//***
+
+/***
+solution
+
+***/
+
+//***
+
+struct checked_policy
+    : basic_policy<checked_policy, checked_perfect_hash<checked_policy>,
+                   throw_error_handler> {};
 
 BOOST_AUTO_TEST_CASE(ref_check_fast_perfect_hash) {
-    std::vector<type_id> ids = {42, 1963, 602701};
-    checked_perfect_hash<throw_policy>::hash_initialize(ids.begin(), ids.end());
-    bool caught = false;
+  std::vector<type_id> ids = {42, 1963, 602701};
+  checked_policy::hash_initialize(ids.begin(), ids.end());
+  bool caught = false;
 
-    try {
-        checked_perfect_hash<throw_policy>::hash_type_id(666);
-    } catch (unknown_class_error& error) {
-        caught = true;
-    }
+  try {
+    checked_policy::hash_type_id(666);
+  } catch (unknown_class_error &error) {
+    caught = true;
+  }
 
-    BOOST_TEST(caught);
+  BOOST_TEST(caught);
 }
+//***
