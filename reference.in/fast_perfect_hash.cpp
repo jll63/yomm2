29bec1abc450ff05a95da0e20e2c36ce46cf366a
Jean-Louis Leroy <jl@leroy.nyc>
2024-02-21 14:29:01 +0800

    doc

--- reference.in/fast_perfect_hash.cpp
+++ reference.in/fast_perfect_hash.cpp
@@ -11,10 +11,10 @@ headers: yorel/yomm2/policy.hpp, yorel/yomm2/core.hpp, yorel/yomm2/keywords.hpp
 
 ---
 ```
-struct checked_fast_perfect_hash;
+struct fast_perfect_hash;
 ```
 
-`fast_perfect_hash` is an implementation of ->`type_hash`. It uses a very fast
+`fast_perfect_hash` is an implementation of ->`type_hash`. It uses the following
 hash function:
 
 ```
@@ -27,6 +27,25 @@ bit size of `type_id`.
 The `M` and `S` coefficients are determined during initialization so that, for
 the given set of type ids, the hash function is _perfect_, i.e. collision-free.
 It is not _minimal_: there may be gaps in the output interval.
+
+`hash_type_id` must not be called with ids not present in the input set passed
+to `hash_initialize`. This happens when the class of a virtual argument was not
+registered via ->`use_classes` or ->`register_classes`. `ref_perfect_hash` will
+silently return an index in the output range, to be used to fetch the v-table
+for the class from a vector. From there, two things can happen. In the lucky
+scenario, the index will correspond to a gap, a wasted entry in the v-table
+vector, which will contain a null pointer. The program will segfault. Such
+mistake is fairly easy to troubleshoot. In the unlucky scenario, there will be a
+collision - the hash function is perfect only for the input ids - and the wrong
+method will be called. The program will carry on, crashing far from the root
+cause of the error, and, in the worst case, not at all.
+
+To help detect such registration errors, it is highly recommended to run debug
+builds of the program with ->`checked_perfect_hash`, which detects missing class
+registrations.
+
+`default_policy` uses `fast_perfect_hash` in release builds, and
+`checked_perfect_hash` in debug builds.
 
 ## Template parameters
 
@@ -46,17 +65,21 @@ template <typename ForwardIterator>
 static size_t hash_initialize(ForwardIterator first, ForwardIterator last)
 ```
 
+Finds the `M` and `S` parameters of the hash function. `ForwardIterator` is an
+iterator that satisfies the requirements of forward iterators; dereferencing it
+yields a `type_id`. Sets `type_hash_last` to the maximum hash value, plus one.
+
 #### Parameters
 
-**first**, **last** - the range
+**first**, **last** - a range of `type_id`s
 
 #### Return value
 
-The size of the zero-based interval containing all the possible output values of
-`hash_type_id`.
+None.
 
+#### Errors
 
-Find
+None.
 
 ### hash_type_id
 
@@ -64,6 +87,21 @@ Find
 template<class Policy>
 static type_id hash_type_id(type_id type)
 ```
+
+Returns the hashed value of `type`, which must be one of the values passed to
+`hash_initialize`.
+
+#### Parameters
+
+**type** - a `type_id`
+
+#### Return value
+
+None.
+
+#### Errors
+
+None.
 
 ### Example
 
@@ -87,6 +125,7 @@ BOOST_AUTO_TEST_CASE(ref_fast_perfect_hash) {
 
   for (auto id : ids) {
     BOOST_TEST_MESSAGE(id << " -> " << fast_hash_policy::hash_type_id(id));
+    // Output:
     // 42 -> 0
     // 1963 -> 3
     // 602701 -> 2
@@ -96,42 +135,10 @@ BOOST_AUTO_TEST_CASE(ref_fast_perfect_hash) {
                   fast_hash_policy::hash_type_id(other)) == (id == other));
     }
   }
-}
-//***
 
-/***
-
-`hash_type_id` must not be called with ids not present in the input set passed
-to `hash_initialize`. This happens when the class of a virtual argument was not
-registered via ->`use_classes` or ->`register_classes`. `ref_perfect_hash` will
-silently return an index in the output range, to be used to fetch the v-table
-for the class from a vector. From there, two things can happen. In the lucky
-scenario, the index will correspond to a gap, a wasted entry in the v-table
-vector, which will contain a null pointer. The program will segfault. Such
-mistake is fairly easy to troubleshoot. In the unlucky scenario, there will be a
-collision - the hash function is perfect only for the input ids - and the wrong
-method will be called. The program will carry on, crashing far from the root
-cause of the error, and, in the worst case, not at all.
-
-The following example demonstrates the problem:
-***/
-
-//***
-
-BOOST_AUTO_TEST_CASE(ref_fast_perfect_hash_problem) {
+  // Calling hash_type_id with an unregistered type_id returns bogus result:
   BOOST_TEST_MESSAGE(666 << " -> " << fast_hash_policy::hash_type_id(666));
   // 666 -> 3 !!!
 }
 
 //***
-
-/***
-
-To help detect such registration errors, it is highly recommended to run debug
-builds of the program with ->`checked_perfect_hash`, which detects missing class
-registrations.
-
-`default_policy` uses `fast_perfect_hash` in release builds, and
-`checked_perfect_hash` in debug builds.
-
-***/
--- reference.in/rtti.cpp
+++ reference.in/rtti.cpp
@@ -5,7 +5,7 @@
 
 <sub>/ ->home / ->reference </sub>
 
-entry: yorel::yomm2::policy::vptr
+entry: yorel::yomm2::policy::rtti
 headers: yorel/yomm2/policy.hpp, yorel/yomm2/core.hpp, yorel/yomm2/keywords.hpp
 
 ---
